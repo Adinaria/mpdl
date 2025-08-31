e36003ca47a4ceb0f0ccb1e71259c17cb83bda28
adinar <adinaria223311@gmail.com>
2025-08-31 21:00:26 +0300

    fix

--- .docker/install.sh
+++ .docker/install.sh
@@ -3,11 +3,3 @@
 if [ ! -d "vendor" ]; then
   composer install --no-interaction --optimize-autoloader --no-dev
 fi
-
-# Копируем .env
-if [ ! -f ".env" ]; then
-    cp .env.example .env
-fi
-
-# Генерируем ключ приложения
-php artisan key:generate
