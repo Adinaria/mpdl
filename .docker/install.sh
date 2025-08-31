648852fb24d860c2e660168d354b45266b70e85f
adinar <adinaria223311@gmail.com>
2025-08-31 21:07:32 +0300

    revert env

--- .docker/install.sh
+++ .docker/install.sh
@@ -3,3 +3,11 @@
 if [ ! -d "vendor" ]; then
   composer install --no-interaction --optimize-autoloader --no-dev
 fi
+
+# Копируем .env
+if [ ! -f ".env" ]; then
+    cp .env.example .env
+fi
+
+# Генерируем ключ приложения
+php artisan key:generate
